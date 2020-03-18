f0bb0de2ddc68e14e87f349645672a2c00ca0920
jarydf <jaryd.fisher@alumni.ubc.caasdfgit config --global --listgit config --global user.name jaryd>
2020-03-18 14:13:40 -0700

    uploaded bootstrap again

--- mywebsite/functions/index.js
+++ mywebsite/functions/index.js
@@ -34,32 +34,3 @@ exports.submit = functions.https.onRequest((req, res) => {
     
   });
 });
-
-// const rp = require("request-promise");
-
-// exports.checkRecaptcha = functions.https.onRequest((req, res) => {
-//   const response = req.query.response;
-//   console.log("recaptcha response", response);
-//   rp({
-//     uri: "https://recaptcha.google.com/recaptcha/api/siteverify",
-//     method: "POST",
-//     formData: {
-//       secret: "6Leme9kUAAAAADv3WwXzMojmiZ9fbISfwl5JMZ3w",
-//       response: response
-//     },
-//     json: true
-//   })
-//     .then(result => {
-//       console.log("recaptcha result", result);
-//       // eslint-disable-next-line promise/always-return
-//       if (result.success) {
-//         res.send("You're good to go, human.");
-//       } else {
-//         res.send("Recaptcha verification failed. Are you a robot?");
-//       }
-//     })
-//     .catch(reason => {
-//       console.log("Recaptcha request failure", reason);
-//       res.send("Recaptcha request failed.");
-//     });
-// });
